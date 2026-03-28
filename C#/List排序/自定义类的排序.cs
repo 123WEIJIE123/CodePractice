70127c599af134273456ad77f6be5fbf681c3766
123WEIJIE123 <2488219565@qq.com>
2026-03-28 18:09:48 +0800

    List排序

---
+++ C#/List排序/自定义类的排序.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List排序
+{
+    
+    class 自定义类的排序
+    {   
+                
+        //static void Main(string[] args)
+        //{ 
+        //    Console.WriteLine("Hello World!");
+        //    #region 知识点二 自定义类的排序
+        //    List<Item> itemList = new List<Item>();
+        //    itemList.Add(new Item(45));
+        //    itemList.Add(new Item(10));
+        //    itemList.Add(new Item(99)); 
+        //    itemList.Add(new Item(24));
+        //    itemList.Add(new Item(100));
+        //    itemList.Add(new Item(12));
+        //    //排序方法
+        //    itemList.Sort();
+        //    for(int i = 0; i < itemList.Count; i++)
+        //    {
+        //        Console.WriteLine(itemList[i].money);
+        //    }
+        //    #endregion
+        //}
+    }
+
+    class Item : IComparable<Item>
+    {
+        public int money;
+
+        public Item(int money)
+        {
+            this.money = money;
+        }
+
+        public int CompareTo(Item other)
+        {   
+            //返回值的含义
+            //小于0
+            //放在传入对象的前面
+            //等于0
+            //保持当前的位置不变
+            //大于0
+            //放在传入对象的后面
+
+            //可以简单理解 传入对象的位置 就是0
+            //如果你的返回值是负数 就放在它的左边 也就是前面
+            //如果你返回正数 就放在它的右边 也就是后边
+
+            if(this.money > other.money)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ C#/List排序/通过委托函数进行排序.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List排序
+{
+    class ShopItem
+    {
+        public int id;
+
+        public ShopItem(int id)
+        {
+            this.id = id;
+        }
+    }
+     
+    class 通过委托函数进行排序
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            #region 知识点三 通过委托函数进行排序
+            List<ShopItem> shopItems = new List<ShopItem>();
+            shopItems.Add(new ShopItem(2));
+            shopItems.Add(new ShopItem(1));
+            shopItems.Add(new ShopItem(4));
+            shopItems.Add(new ShopItem(3));
+            shopItems.Add(new ShopItem(6));
+            shopItems.Add(new ShopItem(5));
+
+            //shopItems.Sort(SortShopItem);
+            shopItems.Sort((a, b) =>
+            {
+                return a.id > b.id ? 1 : -1;
+            });
+            Console.WriteLine("***********************");
+            for(int i = 0; i< shopItems.Count; i++)
+            {
+                Console.WriteLine(shopItems[i].id);
+            }
+            #endregion
+        }
+
+        static int SortShopItem(ShopItem a, ShopItem b)
+        {
+            //传入的两个对象 为列表中的两个对象
+            //进行两两的比较 用左边的和右边条件 比较
+            //返回值规则 和之前一样 0做标准 负数在左(前) 正数在右(后)
+            if(a.id > b.id)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
+
+
+   
+}
