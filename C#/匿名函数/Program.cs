dd88e6cc7aa883d6b4567bd619add440c6a3c45d
123WEIJIE123 <2488219565@qq.com>
2026-03-28 10:40:22 +0800

    匿名函数

---
+++ C#/匿名函数/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 匿名函数
+{
+    class Program
+    {
+        static event Action a = delegate ()
+        {
+            Console.WriteLine("匿名函数逻辑");
+        };
+
+        static void Main(string[] args)
+        {
+            a();
+        }
+    }
+}
