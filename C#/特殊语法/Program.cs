a578c3b20a7e0f3be232bdbdbfb54330a131f57b
123WEIJIE123 <2488219565@qq.com>
2026-03-31 20:13:37 +0800

    迭代器和特殊语法

---
+++ C#/特殊语法/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace 特殊语法
+{   
+    class Person
+    {
+        private int money;
+        public bool sex;
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public Person(int money)
+        {
+            this.money = money;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            #region 知识点二 设置对象初始值
+            //申明对象时
+            //可以通过直接写大括号的形式初始化公共成员变量和属性
+            Person p = new Person(100) { sex = true, Age = 18, Name = "WEIJIE" };
+            Person p2 = new Person(200) { Age = 18 };
+            #endregion
+        }
+    }
+}
