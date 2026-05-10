d174f0dfe7b92a972c08b05d86fca74643cc3e1b
123WEIJIE123 <135416361+123WEIJIE123@users.noreply.github.com>
2026-05-10 12:03:02 +0800

    第一次合并前前手动修改main

--- C#/Learn/Program.cs
+++ C#/Learn/Program.cs
@@ -6,7 +6,7 @@ namespace Learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");//第一次合并前前手动修改main
         }
     }
 }
