bb794588e2acea2c12d5602afac73573bbb4232b
123WEIJIE123 <2488219565@qq.com>
2026-05-10 12:01:06 +0800

    Test分析修改成Console.WriteLine("Hello World!111111");

--- C#/Learn/Program.cs
+++ C#/Learn/Program.cs
@@ -6,7 +6,7 @@ namespace Learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!111111");
         }
     }
 }
