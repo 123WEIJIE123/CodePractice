cd638b201fa16887e8e264db8500b9117ad2fcb5
123WEIJIE123 <135416361+123WEIJIE123@users.noreply.github.com>
2026-05-10 11:53:44 +0800

    还原test

--- C#/Learn/Program.cs
+++ C#/Learn/Program.cs
@@ -7,11 +7,6 @@ namespace Learn
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            //git文件冲突如何解决测试
-            Console.WriteLine("Hello World1111111111!");
-
-            //解决
         }
     }
 }
