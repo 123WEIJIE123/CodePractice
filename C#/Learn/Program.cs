dadc63ec2fd803dbbffc2cd6ec42af22b588e49e
123WEIJIE123 <135416361+123WEIJIE123@users.noreply.github.com>
2026-05-10 11:24:10 +0800

    Update Program.cs

--- C#/Learn/Program.cs
+++ C#/Learn/Program.cs
@@ -6,7 +6,7 @@ namespace Learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1111111111!");
         }
     }
 }
